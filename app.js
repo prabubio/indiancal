f0a5862d8c1463e4c3092f81971d268c6f51c279
Prabu <prabubio@gmail.com>
2013-04-15 12:40:06 +0530

    new data+css changes

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.get('/tamil', function(req, res){
 });
 
 app.listen(3000);
-console.log('Listening on port 3000');
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
+//console.log('Listening on port 3000');

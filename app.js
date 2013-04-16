380bf397b1e333bcf848010c854fa1d9fd6c6e3c
Prabu <prabubio@gmail.com>
2013-04-16 10:14:00 +0530

    redirect root to tamil page

--- app.js
+++ app.js
@@ -1,17 +1,22 @@
-
+//Require
 var express = require('express');
 var app = express();
 var cal = require("./cal/cal.js");
 
+//Routes
 app.use(express.static(__dirname + '/public'));
 app.get('/tamil', function(req, res){
-  var pagename = req.params.pagename;
+    var pagename = req.params.pagename;
     cal.getCal(req, res, pagename);
 });
+app.get('/', function(req, res){
+    res.writeHead(302, {Location: 'http://indiancal.com/tamil'});
+    res.end();
+});
 
-app.listen(3000);
+//Listen
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-//console.log('Listening on port 3000');
+

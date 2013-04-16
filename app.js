ba56bbd352c3d9a424c554599bcf611050bedd5b
Prabu <prabubio@gmail.com>
2013-04-16 16:48:10 +0530

    add console message

--- app.js
+++ app.js
@@ -4,15 +4,19 @@ var app = express();
 var cal = require("./cal/cal.js");
 
 //Routes
+app.get('/', function(req, res){
+    console.log('[LOG] redireting to tamil calendar');
+    res.writeHead(302, {Location: 'http://indiancal.com/tamil'});
+    res.end();
+});
+
 app.use(express.static(__dirname + '/public'));
+
 app.get('/tamil', function(req, res){
     var pagename = req.params.pagename;
     cal.getCal(req, res, pagename);
 });
-app.get('/', function(req, res){
-    res.writeHead(302, {Location: 'http://indiancal.com/tamil'});
-    res.end();
-});
+
 
 //Listen
 var port = process.env.PORT || 5000;
